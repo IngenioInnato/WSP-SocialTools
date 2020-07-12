333c51a0e3d99f15fc91c57be64bf8f63c348b4e
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-07-11 20:54:36 -0400

    Agregados cambios y mejoras

--- js/main.js
+++ js/main.js
@@ -1,17 +1,146 @@
 Vue.component('gallery', {
   template:/*template*/ `
-  <div>
+  <div class="gallery">
+    <div class="gallery__item" v-for="img in imgData" :style="{'background': 'url('+ img.image + ') no-repeat center / cover'}">
+      <button class="btn-orange" @click="shareImg(img.image)">Compartir</button>
+    </div>
   </div>
   `,
-  props: {},
-  data() {
-    return {}
+  props: {
+    // file: {
+    //   type: String,
+    //   required: true,
+    //   default: ""
+    // }
   },
-  methods: {},
-  computed: {}
+  data() {
+    return {
+      imgData: [
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200117.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200120.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200124.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200127.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200131.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200217.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200308.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200311.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200313.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200422.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200510.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200515.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200527.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200530.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200605.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200608.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200615.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200621.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200708.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200710.jpg",
+          "date" : ""
+        }, 
+        {
+          "name" : "",
+          "image": "./img/post-us/fb-20200117.jpg",
+          "date" : ""
+        }
+      ]
+    }
+  },
+  methods: { 
+    shareImg(img){
+      let loc = window.location.pathname;
+      let dir = loc.substring(0, loc.lastIndexOf('/'));
+      let folder= window.location.hostname + dir + "/";
+      let link = folder + img.substr(2);
+      clipboard.writeText(link);
+    }
+  },
+  computed: {
+
+  },
 });
+
+
 let app = new Vue({
-  el: '#app',
+  el: '.app',
   data: {
 
   },
