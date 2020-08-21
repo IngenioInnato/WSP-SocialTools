0d8a7112d5b40295c935b9523d0870bf30967098
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-08-21 00:11:41 -0400

    Agregados cambios y mejoras

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-Vue.component('gallery', {
+let gallery= Vue.component('gallery', {
   template:/*template*/ `
-  <div class="gallery">
-    <div class="gallery__item" v-for="img in imgData" :style="{'background': 'url('+ img.image + ') no-repeat center / cover'}">
-      <button class="btn-orange" @click="shareImg(img.image)">Compartir</button>
+  <div class="layout">
+    <div class="gallery">
+      <div class="gallery__item" v-for="img in imgData.slice(0,n)" :style="{'background': 'url('+ img.image + ') no-repeat center / cover'}">
+        <button class="btn-orange" @click="shareImg(img.image)">Share</button>
+      </div>
+    </div>
+    <div class="mt-3 text-center">
+      <button @click="n -= 9" class="btn-orange" :style="{'display': (n > 9) ? 'inline-block' : 'none'}">Less</button>
+      <button @click="n += 9" class="btn-orange" :style="{'display': (n < imgLength) ? 'inline-block': 'none'}">More</button>
     </div>
   </div>
   `,
@@ -17,7 +23,9 @@ Vue.component('gallery', {
   },
   data() {
     return {
-      imgData: []
+      imgData: [],
+      imgLength: 0,
+      n: 9
     }
   },
   methods: { 
@@ -28,32 +36,24 @@ Vue.component('gallery', {
       let link = folder + img.substr(2);
       clipboard.writeText(link);
     },
-    fillData(){
-      return `
-
-      `
-    },
     getData(){
       // axios
       axios.get("../" + this.file)
       .then(response => {
         // console.log(response); 
-        this.imgData = response.data;
+        this.imgData = response.data.reverse();
+        this.imgLength = this.imgData.length;
       });
     }
   },
   mounted() {
     this.getData();
-  },
-  watch: {
   }
 });
 
-
 let app = new Vue({
   el: '.app',
-  data: {
-
-  },
-  methods: {}
+  components: {
+    'gallery': gallery
+  }
 });
