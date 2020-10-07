c9b0d4e3a3dbab47b448f67f59b0b7d5e2e16e24
MiguelAngel0201 <mpena.creactiva@gmail.com>
2020-10-07 15:31:05 -0400

    Añadida mejora, cambiar estatus de boton al hacer click

--- js/main.js
+++ js/main.js
@@ -5,13 +5,13 @@ let gallery= Vue.component('gallery', {
   <div class="layout">
   <div class="gallery">
   <div class="gallery__item" v-for="img in imgData.slice(0,n)" :style="{'background': 'url('+ img.image + ') no-repeat center / cover'}">
-  <button class="btn-orange btn-ripple" @click="shareImg(img.image)">Share</button>
+  <button class="btn-orange btn-ripple" @click="shareImg($event, img.image)">Share</button>
   </div>
   </div>
   <div class="mt-3 text-center">
   <button @click="n -= 9" class="btn-orange btn-ripple" :style="{'display': (n > 9) ? 'inline-block' : 'none'}">Less</button>
   <button @click="n += 9" class="btn-orange btn-ripple" :style="{'display': (n < imgLength) ? 'inline-block': 'none'}">More</button>
-    </div>
+  </div>
   </div>
   `,
   // https://gist.github.com/dvreed77/c37759991b0723eebef3647015495253
@@ -31,12 +31,13 @@ let gallery= Vue.component('gallery', {
     }
   },
   methods: { 
-    shareImg(img){
+    shareImg(event,img){
       let loc = window.location.pathname;
       let dir = loc.substring(0, loc.lastIndexOf('/'));
       let folder= window.location.hostname + dir + "/";
       let link = folder + img.substr(2);
       clipboard.writeText(link);
+      this.changeStatus(event);
     },
     getData(){
       // axios
@@ -46,6 +47,17 @@ let gallery= Vue.component('gallery', {
         this.imgData = response.data.reverse();
         this.imgLength = this.imgData.length;
       });
+    },
+    changeStatus(el) {
+      let button = el.currentTarget;
+      console.log(button);
+
+      button.innerHTML = "Copied!";
+      button.style.background = "#0067A3"
+      setInterval(() => {
+        button.innerHTML = "Share";
+        button.style.background = "#F88E2A"
+      }, 1500)
     }
   },
   mounted() {
@@ -62,16 +74,16 @@ let app = new Vue({
 
 // Animaciones
 // document.addEventListener("DOMContentLoaded", function() {
-  let btn = document.querySelectorAll(".btn-ripple");
-  btn.forEach(el => {
-    el.addEventListener("click", function(e) {
-      let x = e.clientX - e.target.offsetLeft;
-      let y = e.clientY - e.target.offsetTop;
-      let ripple = document.createElement("span");
-      ripple.style.left = `${x}px`;
-      ripple.style.top = `${y}px`;
-      this.appendChild(ripple);
+  // let btn = document.querySelectorAll(".btn-ripple");
+  // btn.forEach(el => {
+  //   el.addEventListener("click", function(e) {
+  //     let x = e.clientX - e.target.offsetLeft;
+  //     let y = e.clientY - e.target.offsetTop;
+  //     let ripple = document.createElement("span");
+  //     ripple.style.left = `${x}px`;
+  //     ripple.style.top = `${y}px`;
+  //     this.appendChild(ripple);
 
-    });
-  });
+  //   });
+  // });
 // });
